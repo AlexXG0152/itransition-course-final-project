413b8727cbdc507a7a9be823de84cff8a264aafd
Alex <al.gr.0017@gmail.com>
2023-08-22 12:29:49 +0300

    feat: add configuration file, cross-env

---
+++ config/configuration.ts
@@ -0,0 +1,21 @@
+export const configuration = () => ({
+  NODE_ENV: process.env.NODE_ENV,
+
+  PORT: parseInt(process.env.PORT, 10) || 3001,
+
+  MYSQL_DIALECT: 'mysql',
+  MYSQL_HOST: process.env.MYSQL_HOST,
+  MYSQL_PORT: process.env.MYSQL_PORT,
+  MYSQL_USER: process.env.MYSQL_USER,
+  MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+  MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+  MYSQL_POOL_MAX: process.env.MYSQL_POOL_MAX,
+  MYSQL_POOL_MIN: process.env.MYSQL_POOL_MIN,
+  MYSQL_POOL_ACQUIRE: process.env.MYSQL_POOL_ACQUIRE,
+  MYSQL_POOL_IDLE: process.env.MYSQL_POOL_IDLE,
+
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Dialect } from 'sequelize/types/sequelize';
+import { configuration } from 'config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.development',
+      envFilePath: `${process.cwd()}/config/.env.${process.env.NODE_ENV}`,
+      load: [configuration],
     }),
     SequelizeModule.forRoot({
-      dialect: process.env.MYSQL_DIALECT as Dialect,
+      dialect: 'mysql',
       host: process.env.MYSQL_HOST,
       port: Number(process.env.MYSQL_PORT),
       username: process.env.MYSQL_USER,
