f5f2fa8fabbf4494af53ef015272a4158e432d46
Alex <al.gr.0017@gmail.com>
2023-08-22 12:05:01 +0300

    feat: install Sequlize, connect MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Dialect } from 'sequelize/types/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+    }),
+    SequelizeModule.forRoot({
+      dialect: process.env.MYSQL_DIALECT as Dialect,
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
